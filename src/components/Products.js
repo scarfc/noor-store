66ed828f87318764e0620ea2ef72eb62b60c6404
scarfc <nour.dellel@gmail.com>
2021-12-24 10:58:43 +0100

    Products list add

---
+++ src/components/Products.js
@@ -0,0 +1,89 @@
+import { Container } from '@material-ui/core';
+import { SettingsSystemDaydreamTwoTone } from '@material-ui/icons';
+import React, { useState, useEffect } from 'react'
+
+function Products() {
+
+    const [data, setData] = useState([]);
+    const [filter, setFilter] = useState(data);
+    const [loading, setLoading] = useState(false);
+    let componentMounted = true;
+
+    useEffect(() => {
+        const getProducts = async () => {
+            setLoading(true);
+            const response = await fetch("https://fakestoreapi.com/products");
+            if (componentMounted) {
+                setData(await response.clone().json());
+                setFilter(await response.json());
+                setLoading(false);
+                console.log(filter)
+            }
+
+            return () => {
+                componentMounted = false;
+            }
+        }
+
+        getProducts();
+    }, []);
+
+    const Loading = () => {
+        return (
+            <>
+                Loading...
+            </>
+        );
+    };
+    const ShowProducts = () => {
+        return (
+            <>
+                <div className="buttons d-flex justify-content-center mb-5 pb-5">
+                    <button className="btn btn-outline-dark">All</button>
+                    <button className="btn btn-outline-dark me-2">Men's Clothing</button>
+                    <button className="btn btn-outline-dark me-2">Women's Clothing</button>
+                    <button className="btn btn-outline-dark me-2">Jewelery</button>
+                    <button className="btn btn-outline-dark me-2">Electronics</button>
+                </div>
+                {filter.map((product) => {
+                    return (
+                        <>
+                            <div className="col-md-3">
+                                <div class="card">
+                                    <img src={product.image} class="card-img-top" alt={product.title}/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">{product.title}</h5>
+                                            <p class="card-text">
+                                                ${product.price}
+                                            </p>
+                                            <a href="#" class="btn btn-primary">Add</a>
+                                        </div>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })}
+            </>
+        );
+
+    };
+    return (
+        <div>
+            <Container>
+                <div className="container my-5 py-5">
+                    <div className="row">
+                        <div className="col-12 mb-5">
+                            <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row justify-content-center">
+                        {loading ? <Loading /> : <ShowProducts />}
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Products
