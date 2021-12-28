ec2a41c285be3175ab445e6d2146576822605bd9
scarfc <nour.dellel@gmail.com>
2021-12-28 11:11:04 +0100

    Fetching data with Axios

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import Announcement from '../components/Announcement'
-import Navbar from '../components/Navbar'
-import Products from '../components/Products'
-import Slider from '../components/Slider'
+import React from 'react';
+import Announcement from '../components/Announcement';
+import Navbar from '../components/Navbar';
+import Products from '../components/Products';
+import Slider from '../components/Slider';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const Home = () => {
     return (
+        <Router>
         <div>
             <Announcement/>
             <Navbar/>
-            <Products/>
+            <Products />
             <Slider/>
         </div>
+        </Router>
     )
 }
 
 export default Home
+ 
